39f8d790c547ec7aecf13a1ed7b10f1a649b91df
ShahafSY <shahaf@skywiz.io>
2018-12-13 13:26:46 +0000

    Network directroy changes

--- networking/variables.tf
+++ networking/variables.tf
@@ -3,4 +3,12 @@
 variable "vpc_cidr" {
     default = "10.123.0.0/16"
 }
-    
+
+variable "public_cidrs" { default = ["bla", "bla"] }
+
+#variable = "public_cidrs"{
+#    type = "list"
+#    default = ["10.123.1.0/24","10.123.2.0/24"]
+#}
+
+#variable = "accessip" { default = ["0.0.0.0/0"] }
